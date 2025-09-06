1ace6a1226864e280b87988cfa605c4f6816bb14
Pete Letkeman <pete@letkeman.ca>
2025-09-05 21:17:47 -0400

    export classes

---
+++ src/jsonata/Timebox/__init__.py
@@ -0,0 +1,12 @@
+import os
+import glob
+
+__all__ = []
+
+# Get all .py files in the current directory except __init__.py
+module_files = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
+for path in module_files:
+    module = os.path.splitext(os.path.basename(path))[0]
+    if module != "__init__":
+        __all__.append(module)
+        exec(f"from .{module} import *")
